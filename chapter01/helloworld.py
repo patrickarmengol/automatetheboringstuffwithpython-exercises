ad747334433adab272a714156f492ae505a6b391
Patrick Armengol <patrickarmengol@protonmail.com>
2022-01-12 11:46:39 -0500

    chapter01

---
+++ chapter01/helloworld.py
@@ -0,0 +1,11 @@
+# says hello and asks for name
+
+print('hello, world!')
+print('what is your name?')
+my_name = input()
+print('it is good to meet you, ' + my_name)
+print('the length of your name is: ')
+print(len(my_name))
+print('what is your age?')
+my_age = input()
+print('you will be ' + str(int(my_age) + 1) + ' in a year.')
